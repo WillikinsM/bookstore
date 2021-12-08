9477bbeaf8da886d1f8e24d21ef3990e0eb1b984
WillikinsM <willikins98@gmail.com>
2021-12-08 17:16:01 -0300

    implemented the domain classes as entities

--- src/main/java/com/will/bookstoreapi/domain/Book.java
+++ src/main/java/com/will/bookstoreapi/domain/Book.java
@@ -1,7 +1,16 @@
 package com.will.bookstoreapi.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Book implements Serializable {
 
 	/**
@@ -9,11 +18,15 @@ public class Book implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
 	private String authorName;
 	private String text;
 
+	@ManyToOne
+	@JoinColumn(name = "category_id")
 	private Category category;
 
 	public Book() {
@@ -67,6 +80,23 @@ public class Book implements Serializable {
 
 	public void setCategory(Category category) {
 		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
--- src/main/java/com/will/bookstoreapi/domain/Category.java
+++ src/main/java/com/will/bookstoreapi/domain/Category.java
@@ -1,30 +1,44 @@
 package com.will.bookstoreapi.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class Category {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+@Entity
+public class Category implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	private String name;
 	private String description;
 
+	@OneToMany(mappedBy = "category")
 	private List<Book> book = new ArrayList<>();
 
 	public Category() {
 		super();
 	}
-	
-	
-	
+
 	public Category(Integer id, String name, String description) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
 	}
-
-
 
 	public Integer getId() {
 		return id;
@@ -57,8 +71,22 @@ public class Category {
 	public void setBook(List<Book> book) {
 		this.book = book;
 	}
-	
-	
-	
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+
 }
